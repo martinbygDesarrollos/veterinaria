5c9344be2a32ef32e8d855f838020ed3949f181e
Martin B&G Desarrollo de Sistemas <martin@hit.com.uy>
2021-09-16 15:07:36 -0300

    btnInicio

--- public/scripts/iniciarSesion.js
+++ public/scripts/iniciarSesion.js
@@ -10,4 +10,14 @@ function iniciarSesion(){
 			else showReplyMessage(response.result, response.message, "Iniciar sesisón", null);
 		}else showReplyMessage(1, "Debe ingresar la contraseña para iniciar sesión", "Contraseña campo requerido", null);
 	}else showReplyMessage(1, "Debe ingresar un usuario para iniciar sesión", "Usuario campo requerido", null);
+	$('#inpUsuario').focus();
+}
+
+function keyPressSingIn(eventEnter){
+	if(eventEnter.keyCode == 13){
+		if(eventEnter.srcElement.id == "inpUsuario")
+			$('#inpPass').focus();
+		else if(eventEnter.srcElement.id == "inpPass")
+			$('#btnConfirm').click();
+	}
 }
