cb14b153ebfcd1730824e68bd03d21315a214226
Martin B&G Desarrollo de Sistemas <martin@hit.com.uy>
2022-05-27 16:36:18 -0300

    mensajes

---
+++ src/utils/messages.php
@@ -0,0 +1,15 @@
+<?php
+define("message_error", "Ha ocurrido un error, no se obtuvo respuesta de la base de datos.");
+
+define("no_data", "No se han encontrado datos para mostrar.");
+define("no_update_data", "No se ha podido actualizar los datos.");
+define("no_select_data", "No se ha podido hacer la consulta.");
+define("no_data_to_update","No se encontraron datos para actualizar.");
+define("no_add_files", "No se ha podido cargar archivos.");
+define("no_insert_data", "No se ha podido guardar los datos.");
+define("no_session", "Por favor, inicie sesión nuevamente.");
+
+define("ok_update_data", "Los datos se actualizaron correctamente.");
+define("ok_insert_data", "Los datos se han guardado correctamente.");
+
+?>
