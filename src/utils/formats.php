f5f52f006e7c394474112054ce8950b27b4bffc1
Martin B&G Desarrollo de Sistemas <martin@hit.com.uy>
2022-05-27 16:35:55 -0300

    formato hora

--- src/utils/formats.php
+++ src/utils/formats.php
@@ -8,4 +8,12 @@ class formats{
 			return substr($ci, 0, 1) . "." . substr($ci, 1, 3) . "." . substr($ci, 4, 3) . "-" . substr($ci, 7, 1);
 		else return $ci;
 	}
+
+	//ingresa 1030 devuelve 10:30
+	public function formatStringToTime($string){
+		if (strlen($string) != 4){
+			return false;
+		}else
+			return substr($string, 0, 2) . ":" . substr($string, 2, 2);
+	}
 }
