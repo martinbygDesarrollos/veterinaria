59a0a9dc5c0efdba741ba0cab6e1de479dff66a0
Martin B&G Desarrollo de Sistemas <martin@hit.com.uy>
2022-06-03 11:34:18 -0300

    mostrar o no el boton de enviar por wpp analisis si el socio tiene tel o no

--- public/scripts/analisis.js
+++ public/scripts/analisis.js
@@ -178,8 +178,12 @@ function verAnalisis(idAnalisis){
 			$("#thSendFilesTableModalView").attr("hidden", false);
 			$("#thSendFilesTableModalView").attr("disable", false);
 
+			buttonWhatsapp = '<td class="text-center" title="No se encontró número de teléfono"></td>';
+			if ( phoneSocio )
+				buttonWhatsapp = '<td class="text-center"><a href="https://wa.me/'+phoneSocio+'" target="_blank"><button title="Enviar archivo '+phoneSocio+'" class="btn bg-light"><i class="fab fa-whatsapp"></i></button></a></td>';
+
 			for (var i = 0; i < analisis.archivos.length; i++) {
-				let row = '<tr><td>'+analisis.archivos[i].nombre+'</td><td class="text-center"><button title="Descargar archivo"class="btn bg-light" onclick="downloadFile('+analisis.archivos[i].idMedia+')"><i class="fas fa-download"></i></button></td><td class="text-center"><a href="https://wa.me/'+phoneSocio+'" target="_blank"><button title="Enviar archivo '+phoneSocio+'" class="btn bg-light"><i class="fab fa-whatsapp"></i></button></a></td></tr>';
+				let row = '<tr><td>'+analisis.archivos[i].nombre+'</td><td class="text-center"><button title="Descargar archivo"class="btn bg-light" onclick="downloadFile('+analisis.archivos[i].idMedia+')"><i class="fas fa-download"></i></button></td>'+buttonWhatsapp+'</tr>';
 
 				$("#divFilesTableModalView table tbody").append(row);
 			}
