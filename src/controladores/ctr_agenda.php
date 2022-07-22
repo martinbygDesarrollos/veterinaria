414f30194c08b750f12e2ce67dad1a54525fddbf
Martin B&G Desarrollo de Sistemas <martin@hit.com.uy>
2022-07-22 12:09:58 -0300

    mostrar correctamente los datos luego de guardarlos

--- src/controladores/ctr_agenda.php
+++ src/controladores/ctr_agenda.php
@@ -1,7 +1,9 @@
 <?php
 
 require_once '../src/clases/agenda.php';
-require_once '../src/clases/agenda.php';
+require_once '../src/controladores/ctr_usuarios.php';
+require_once '../src/controladores/ctr_mascotas.php';
+
 
 class ctr_agenda {
 
@@ -10,6 +12,8 @@ class ctr_agenda {
 		$response = new \stdClass();
 		$calendarClass = new agenda();
 		$formatClass = new formats();
+		$usersController = new ctr_usuarios();
+		$petController = new ctr_mascotas();
 
 		$day = str_replace("-", "", $day);
 
@@ -30,6 +34,32 @@ class ctr_agenda {
 
 				if (!$value['descripcion'])
 					$response->listResult[$index]['descripcion'] = "";
+
+				$response->listResult[$index]['socio'] = null;
+				if ( isset($value['idSocio'])){
+					if ( ctype_digit($value['idSocio']) ){
+						$resultClient = $usersController->getSocio($value['idSocio']);
+						if ( $resultClient->result == 2 ){
+							$response->listResult[$index]['socio'] = $resultClient->socio;
+							$response->listResult[$index]['nombreCliente'] = $resultClient->socio->idSocio." - ".$resultClient->socio->nombre;
+						}
+					}else
+						$response->listResult[$index]['nombreCliente'] = $value['idSocio'];
+				}else $response->listResult[$index]['nombreCliente'] = "";
+
+
+				$response->listResult[$index]['mascota'] = null;
+				if ( isset($value['idMascota'])){
+					if ( ctype_digit($value['idMascota']) ){
+						$resultPet = $petController->getMascota($value['idMascota']);
+						//var_dump($resultPet);exit;
+						if ( $resultPet->result == 2 ){
+							$response->listResult[$index]['mascota'] = $resultPet->objectResult;
+							$response->listResult[$index]['nombreMascota'] = $resultPet->objectResult->idMascota." - ".$resultPet->objectResult->nombre;
+						}
+					}else
+						$response->listResult[$index]['nombreMascota'] = $value['idMascota'];
+				}else $response->listResult[$index]['nombreMascota'] = "";
 			}
 		}else if ( $response->result == 1 ){
 			$response->result = 2;
